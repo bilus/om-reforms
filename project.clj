f7602e154488da3042829fff0845ddfe6a97380b
Martin Bilski <martinb@criticue.com>
2015-09-27 20:50:09 +0200

    Upgrade cljs and other libs.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
             :url "http://bilus.github.io/reforms/"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                           [org.clojure/clojurescript "0.0-3308"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
+                           [org.clojure/clojurescript "1.7.122"]
                            ; [reforms "0.4.0-SNAPSHOT"]
                            [org.omcljs/om "0.8.8" :scope "provided"]
                            [sablono "0.3.4"]]
@@ -12,8 +12,8 @@
             :auto-clean false
             :source-paths ["src"]
 
-            :plugins [[lein-cljsbuild "1.0.6"]
-                      [lein-doo "0.1.1-SNAPSHOT"]]
+            :plugins [[lein-cljsbuild "1.1.0"]
+                      [lein-doo "0.1.4"]]
 
             :clean-targets ["examples/hello_world/out"
                             "examples/controls/out"
@@ -22,36 +22,37 @@
                             "examples/validation/out"
                             "target/"]
 
-            :profiles {:dev {:dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]}}
+            :profiles {:dev {:dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                             :plugins [[lein-figwheel "0.4.0"]]}}
 
             :aliases {"cljsbuild" ["with-profile" "dev" "cljsbuild"]}
 
             :cljsbuild {:builds {:hello-world
-                                 {:source-paths ["examples/shared/" "examples/hello_world/src" "src" #_ "../reforms/src"]
+                                 {:source-paths ["examples/shared/" "examples/hello_world/src" "src" "../reforms/src"]
                                   :compiler     {:output-to     "examples/hello_world/out/main.js"
                                                  :output-dir    "examples/hello_world/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :controls
-                                 {:source-paths ["examples/shared/" "examples/controls/src" "src"]
+                                 {:source-paths ["examples/shared/" "examples/controls/src" "src" "../reforms/src"]
                                   :compiler     {:output-to     "examples/controls/out/main.js"
                                                  :output-dir    "examples/controls/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :simple
-                                 {:source-paths ["examples/shared/" "examples/simple/src" "src"]
+                                 {:source-paths ["examples/shared/" "examples/simple/src" "src" "../reforms/src"]
                                   :compiler     {:output-to     "examples/simple/out/main.js"
                                                  :output-dir    "examples/simple/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :progress
-                                 {:source-paths ["examples/shared/" "examples/progress/src" "src"]
+                                 {:source-paths ["examples/shared/" "examples/progress/src" "src" "../reforms/src"]
                                   :compiler     {:output-to     "examples/progress/out/main.js"
                                                  :output-dir    "examples/progress/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :validation
-                                 {:source-paths ["examples/shared/" "examples/validation/src" "src" #_"../reforms/src"]
+                                 {:source-paths ["examples/shared/" "examples/validation/src" "src" "../reforms/src"]
                                   :compiler     {:output-to     "examples/validation/out/main.js"
                                                  :output-dir    "examples/validation/out"
                                                  :source-map    true
